9bc1e4372d1695b9c0bd0bb5cb465c4ce714976d
Valentin Dequidt <valentin.dequidt@cegetel.net>
2022-05-31 19:02:52 +0200

    Commit Initial

---
+++ main.py
@@ -0,0 +1,10 @@
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def print_hi(name):
+
+    print(f'Ceci est le début de la programmation de l\'automate cellulaire de {name}')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('John Conway')
