31bf93183fe2125bbf0092c35b09448854a9c9b6
Valentin Dequidt <valentin.dequidt@cegetel.net>
2022-05-31 20:44:57 +0200

    Ajout du controleur d'entrée/sortie utilisateur

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from controleur import *
 
-def print_hi(name):
+automate_etat_1 = []
+automate_etat_2 = []
 
-    print(f'Ceci est le début de la programmation de l\'automate cellulaire de {name}')
+def initialiser_automate(largeur, hauteur):
 
+    iteration_largeur = 0
+    while iteration_largeur < largeur:
+        liste_hauteur = []
+        iteration_hauteur = 0
+        while iteration_hauteur < hauteur:
+            liste_hauteur.append("x")
+            iteration_hauteur += 1
+        automate_etat_1.append(liste_hauteur)
+        iteration_largeur += 1
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('John Conway')
+
+    dimensions_automate = demander_taille_automate()
+    initialiser_automate(dimensions_automate[0], dimensions_automate[1])
+    afficher_automate(automate_etat_1)
+
