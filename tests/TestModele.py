5b96386ef08a333251e8bef203543ba0995c92eb
Valentin Dequidt <valentin.dequidt@cegetel.net>
2022-06-03 21:02:39 +0200

    ajout d'un package de test unitaire

---
+++ tests/TestModele.py
@@ -0,0 +1,20 @@
+import unittest
+
+import modele
+
+
+class TestModele(unittest.TestCase):
+
+    def test_initialiser_automate_1(self):
+        automate = modele.initialiser_automate(-1, -1)
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+        unittest.main()
